1fb0c92410afc88937aef57524df4b937610ba90
Allan Breno <allanbrenogomesdias@gmail.com>
2025-01-21 11:26:54 -0300

    Iniciando repositório

---
+++ src/test/java/com/wakandas/avaliacaoBackend/AvaliacaoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wakandas.avaliacaoBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
